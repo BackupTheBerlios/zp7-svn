c09d92dc9db07869446eaa207ebdac5bf767a2ba
jena <jena@b4e1c19d-8504-0410-bfc1-910685b3975c>
2005-12-05 22:30:37 +0000

    hlavni program

---
+++ trunk/zp7/zpevnikator.py
@@ -0,0 +1,26 @@
+import os,os.path,sys
+
+# import all external modules
+import wx
+import wx.lib.layoutf
+import wx.lib.stattext
+import wx.lib.buttons 
+import wx.grid
+import wx.stc
+import pysqlite2.dbapi2
+import lxml.etree, lxml._elementpath
+import urllib
+import gzip
+import xml.sax.saxutils
+import shutil
+import zipfile
+
+sys.path.append(os.path.normpath('%s/../lib' % sys.argv[0]))
+
+if __name__=='__main__':
+  import update
+  update.update()
+  
+  import main
+  main.main()
+ 
