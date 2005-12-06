4e74aacb2edf2661fb166c2e28800c895e8b4be8
jena <jena@b4e1c19d-8504-0410-bfc1-910685b3975c>
2005-12-06 22:31:36 +0000

    buildovaci skript - vytvareni zp7.zip

--- trunk/zp7/builder/build_zp7.py
+++ trunk/zp7/builder/build_zp7.py
@@ -12,12 +12,21 @@ def emptydir(dirname):
         os.rmdir(os.path.join(root, name))
     os.rmdir(dirname);
 
+noinc=False
+
+for arg in sys.argv[1:]:
+  if arg=='--noinc': noinc=True
+  else: raise Exception('Unknown argument: %s' % arg)
+
 emptydir('build')
 os.mkdir('build')
 os.chdir('build')
 
 os.mkdir("zp7")
-os.system("svn co svn://svn.berlios.de/zp7/trunk/zp7")
+
+os.system("svn checkout https://jena@svn.berlios.de/svnroot/repos/zp7/trunk/zp7")
+os.system("svn checkout https://jena@svn.berlios.de/svnroot/repos/zp7/trunk/winlib")
+#os.system("svn co svn://svn.berlios.de/zp7/trunk/zp7")
 
 print 'creating version'
 version=map(int,open('zp7/lib/version.txt','r').readline().split('.'))
@@ -37,16 +46,45 @@ for root, dirs, files in os.walk("zp7\\lib"):
       zip.write(os.path.join(root,name),os.path.join(root,name).replace("zp7\\",""))
 zip.close()
 
-print 'logging into FTP'
+print 'creating zpevnikator.exe'
+os.chdir('zp7')
+os.system('makeexe.cmd')
+os.chdir('..')
+print 'creating zp7.zip'
+zip=zipfile.ZipFile('zp7.zip',"w",zipfile.ZIP_DEFLATED)
+zip.writestr('zp7/','')
+zip.writestr('zp7/auto-install/','')
+zip.writestr('zp7/db/','')
+zip.writestr('zp7/lib/','')
+zip.writestr('zp7/lib/update.py',open('zp7/lib/update.py').read())
+zip.writestr('zp7/library.zip',open('zp7/dist/library.zip','rb').read())
+zip.writestr('zp7/MSVCR71.dll',open('zp7/dist/MSVCR71.dll','rb').read())
+zip.writestr('zp7/w9xpopen.exe',open('zp7/dist/w9xpopen.exe','rb').read())
+zip.writestr('zp7/zpevnikator.exe',open('zp7/dist/zpevnikator.exe','rb').read())
+zip.writestr('zp7/unicows.dll',open('winlib/unicows.dll','rb').read())
+zip.writestr('zp7/options.xml',open('zp7/options.xml').read())
+zip.writestr('zp7/auto-install/lib.zip',open('lib.zip','rb').read())
+zip.writestr('zp7/auto-install/version.txt',version)
+zip.close()
+
+print 'logging into FTP autoinstall'
 ftp=ftplib.FTP("ftp.zpevnik.net")
-ftp.login(user="zpevnik.net",passwd="u55XhSPF")
+ftp.login(user="autoinstall.zpevnik.net",passwd="python")
 print 'uploading lib.zip'
-ftp.storbinary("STOR /www/auto-install/lib.zip",open('lib.zip','rb'));
+ftp.storbinary("STOR /lib.zip",open('lib.zip','rb'));
 print 'uploading version.txt'
-ftp.storbinary("STOR /www/auto-install/version.txt",StringIO(version))
+ftp.storbinary("STOR /version.txt",StringIO(version))
 ftp.quit()
 
-os.system('svn ci zp7/lib/version.txt -m "automatic version increase"')
+print 'logging into FTP download'
+ftp=ftplib.FTP("ftp.zpevnik.net")
+ftp.login(user="download.zpevnik.net",passwd="python")
+print 'uploading zp7.zip'
+ftp.storbinary("STOR /zp7.zip",open('zp7.zip','rb'));
+ftp.quit()
+
+if not noinc:
+  os.system('svn ci zp7/lib/version.txt -m "automatic version increase"')
 
 print 'finished OK, new version: %s' % version
 
