06b75f1fdaa3a27d62059bcf933cc13de1165e6b
jena <jena@b4e1c19d-8504-0410-bfc1-910685b3975c>
2007-07-28 10:15:38 +0000

    vymazani starych nepotrebnych souboru

--- zp8/zp8/Database/SongServer.cs
+++ zp8/zp8/Database/SongServer.cs
@@ -1,116 +1,69 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Net;
 using System.IO;
+using System.Reflection;
 using System.Xml;
 using System.Xml.Serialization;
+using System.Net;
 using System.ComponentModel;
 
-#if false
 namespace zp8
 {
-    public interface ISongServerType
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple=false)]
+    public class SongServerAttribute : Attribute
     {
-        ISongServer Load(string url, string config);
-        string Name { get;}
-        bool Readonly { get;}
+        public string Title;
+        public string Description;
+        public string Name;
+        public bool ReadOnly = true;
     }
+
+    public class SongServerType
+    {
+        public Type Type;
+        public string Title;
+        public string Description;
+        public string Name;
+        public bool ReadOnly;
+
+        public ISongServer CreateServer()
+        {
+            return (ISongServer)Type.GetConstructor(new Type[] { }).Invoke(new object[] { });
+        }
+    }
+
+    /*
+    public struct SongServerInfo
+    {
+        public string URL;
+        public string Type;
+        public string Config;
+    }
+    */
 
     public interface ISongServer
     {
-        string URL { get;}
-        string Type { get;}
-        string Config { get;}
+        //string URL { get;}
+        //string Type { get;}
+        //string Config { get;}
         void DownloadNew(AbstractSongDatabase db, int serverid);
         void UploadChanges(AbstractSongDatabase db, int serverid);
         void UploadWhole(AbstractSongDatabase db, int serverid);
+        //void Save(out SongServerInfo dst);
+        //void Load(SongServerInfo src);
     }
 
-    public interface ISongServerFactoryType
+    public abstract class BaseSongServer : zp8.PropertyPageBase, ISongServer
     {
-        ISongServerFactory CreateFactory();
-        string Name { get;}
-        string Description { get;}
-    }
-
-    public interface ISongServerFactory
-    {
-        bool Work(List<ISongServer> servers, out string message);
-    }
-
-    public static class SongServer
-    {
-        static Dictionary<string, ISongServerType> m_types = new Dictionary<string, ISongServerType>();
-        static Dictionary<string, ISongServerFactoryType> m_ftypes = new Dictionary<string, ISongServerFactoryType>();
-
-        public static ISongServer LoadSongServer(string type, string url, string config)
-        {
-            return m_types[type].Load(url, config);
-        }
-        public static void RegisterSongServer(ISongServerType type)
-        {
-            m_types[type.Name] = type;
-        }
-        public static void RegisterFactory(ISongServerFactoryType type)
-        {
-            m_ftypes[type.Name] = type;
-        }
-
-        public static IEnumerable<ISongServerType> GetTypes()
-        {
-            return m_types.Values;
-        }
-        public static IEnumerable<ISongServerFactoryType> GetFactoryTypes()
-        {
-            return m_ftypes.Values;
-        }
-        public static ISongServerType ServerType(string name) { return m_types[name]; }
-
-        static SongServer()
-        {
-            RegisterSongServer(new XmlSongServerType());
-            RegisterSongServer(new FtpSongServerType());
-            RegisterSongServer(new FileSongServerType());
-            RegisterFactory(new XmlSongServerFactoryType());
-            RegisterFactory(new FtpSongServerFactoryType());
-            RegisterFactory(new FileSongServerFactoryType());
-        }
-    }
-
-    public abstract class BaseSongServer : ISongServer
-    {
-        protected string m_url;
-        protected string m_type;
-        protected string m_config;
-
-        public BaseSongServer(string type, string url, string config)
-        {
-            m_url = url;
-            m_type = type;
-            m_config = config;
-        }
-
         #region ISongServer Members
 
-        public string URL
+        public virtual void DownloadNew(AbstractSongDatabase db, int serverid)
         {
-            get { return m_url; }
+            throw new Exception("The method or operation is not implemented.");
         }
 
-        public string Type
-        {
-            get { return m_type; }
-        }
-
-        public string Config
-        {
-            get { return m_config; }
-        }
-
-        public abstract void DownloadNew(AbstractSongDatabase db, int serverid);
-
-        public virtual void UploadChanges(AbstractSongDatabase songDatabase, int serverid)
+        public virtual void UploadChanges(AbstractSongDatabase db, int serverid)
         {
             throw new Exception("The method or operation is not implemented.");
         }
@@ -123,104 +76,131 @@ namespace zp8
         #endregion
     }
 
-    public class XmlSongServerType : ISongServerType
+    public abstract class ReadOnlySongServer : BaseSongServer
     {
-        #region ISongServerType Members
-
-        public ISongServer Load(string url, string config)
-        {
-            return new XmlSongServer(url);
-        }
-
-        public string Name
-        {
-            get { return "xml"; }
-        }
-
-        public bool Readonly
-        {
-            get { return true; }
-        }
-
-        #endregion
-    }
-
-    public class XmlSongServer : BaseSongServer
-    {
-        public XmlSongServer(string url) : base("xml", url, null) { m_url = url; }
+        protected abstract Stream Read(ref object request);
+        protected virtual void CloseRead(object request) { }
 
         public override void DownloadNew(AbstractSongDatabase db, int serverid)
         {
-            WebRequest req = WebRequest.Create(m_url);
-            WebResponse resp = req.GetResponse();
-            using (Stream fr = resp.GetResponseStream())
+            //db.DeleteSongsFromServer(serverid);
+            object request = null;
+            using (Stream fr = Read(ref request))
             {
                 db.MergeInternetXml(serverid, fr);
             }
-            resp.Close();
+            CloseRead(request);
         }
     }
 
-    public class XmlSongServerFactory : PropertyPageBase, ISongServerFactory
+    public abstract class ReadWriteSongServer : ReadOnlySongServer
+    {
+        protected abstract Stream Write(ref object request);
+        protected virtual void CloseWrite(object request) { }
+
+        public override void UploadChanges(AbstractSongDatabase db, int serverid)
+        {
+            InetSongDb xmldb = new InetSongDb();
+            object req1 = null;
+            using (Stream fr = Read(ref req1)) xmldb.ReadXml(fr);
+            CloseRead(req1);
+
+            db.UpdateInternetXml(serverid, xmldb);
+
+            object req2 = null;
+            using (Stream fw = Write(ref req2))
+            {
+                xmldb.WriteXml(fw);
+            }
+            CloseWrite(req2);
+        }
+        public override void UploadWhole(AbstractSongDatabase db, int serverid)
+        {
+            object request = null;
+            using (Stream fw = Write(ref request))
+            {
+                db.CreateInternetXml(serverid, fw);
+            }
+            CloseWrite(request);
+        }
+    }
+
+    public static class SongServer
+    {
+        static Dictionary<string, SongServerType> m_types = new Dictionary<string, SongServerType>();
+        static Dictionary<Type, SongServerType> m_invtypes = new Dictionary<Type, SongServerType>();
+
+        static SongServer()
+        {
+            foreach (Type type in Assembly.GetAssembly(typeof(SongServer)).GetTypes())
+            {
+                foreach(SongServerAttribute attr in type.GetCustomAttributes(typeof(SongServerAttribute), true))
+                {
+                    SongServerType st = new SongServerType();
+                    st.Type = type;
+                    st.Description = attr.Description;
+                    st.Name = attr.Name;
+                    st.Title = attr.Title;
+                    st.ReadOnly = attr.ReadOnly;
+                    m_types[attr.Name] = st;
+                    m_invtypes[type] = st;
+                }
+            }
+        }
+
+        public static ISongServer Load(string type, string config)
+        {
+            SongServerType st = m_types[type];
+            XmlSerializer xs = new XmlSerializer(st.Type);
+            using (StringReader sr = new StringReader(config))
+            {
+                return (ISongServer)xs.Deserialize(sr);
+            }
+        }
+
+        public static string Save(ISongServer server)
+        {
+            XmlSerializer xs = new XmlSerializer(server.GetType());
+            using (StringWriter sw = new StringWriter())
+            {
+                xs.Serialize(sw, server);
+                return sw.ToString();
+            }
+        }
+        public static IEnumerable<SongServerType> GetServerTypes()
+        {
+            return m_types.Values;
+        }
+        public static SongServerType GetServerName(ISongServer server)
+        {
+            return m_invtypes[server.GetType()];
+        }
+    }
+
+    [SongServer(Name = "xml", Title = "XML", Description = "XML soubor nìkde na internetu", ReadOnly = true)]
+    public class XmlSongServer : ReadOnlySongServer
     {
         string m_url;
 
         [Description("Internetová adresa XML souboru, napø. http://nejakyweb.cz/muj_zpevnik.xml")]
         public string URL { get { return m_url; } set { m_url = value; } }
-        #region ISongServerFactory Members
 
-        public bool Work(List<ISongServer> servers, out string message)
+        protected override Stream Read(ref object request)
         {
-            servers.Add(new XmlSongServer(URL));
-            message = "Pøidáno:" + URL;
-            return true;
+            WebRequest req = WebRequest.Create(m_url);
+            WebResponse resp = req.GetResponse();
+            request = resp;
+            return resp.GetResponseStream();
         }
 
-        #endregion
-    }
-
-    public class XmlSongServerFactoryType : ISongServerFactoryType
-    {
-        #region ISongServerFactoryType Members
-
-        public ISongServerFactory CreateFactory()
+        protected override void CloseRead(object request)
         {
-            return new XmlSongServerFactory();
+            ((WebResponse)request).Close();
         }
-
-        public string Name
+        public override string ToString()
         {
-            get { return "xml"; }
+            return m_url;
         }
-
-        public string Description
-        {
-            get { return "XML soubor nìkde na internetu"; }
-        }
-
-        #endregion
-    }
-
-    public class FtpSongServerType : ISongServerType
-    {
-        #region ISongServerType Members
-
-        public ISongServer Load(string url, string config)
-        {
-            return new FtpSongServer(url, config);
-        }
-
-        public string Name
-        {
-            get { return "ftp"; }
-        }
-
-        public bool Readonly
-        {
-            get { return false; }
-        }
-
-        #endregion
     }
 
     public class FtpAccess
@@ -270,65 +250,21 @@ namespace zp8
         }
     }
 
-    public abstract class ReadWriteServer : BaseSongServer
+    [SongServer(Name = "ftp", Title = "FTP", Description = "XML soubor uložený na FTP (ètení/zápis)", ReadOnly=false)]
+    public class FtpSongServer : ReadWriteSongServer
     {
-        public ReadWriteServer(string type, string url, string config)
-            : base(type, url, config)
-        {
-        }
+        FtpAccess m_access = new FtpAccess();
 
-        protected abstract Stream Read(ref object request);
-        protected virtual void CloseRead(object request) { }
-        protected abstract Stream Write(ref object request);
-        protected virtual void CloseWrite(object request) { }
-
-        public override void DownloadNew(AbstractSongDatabase db, int serverid)
-        {
-            //db.DeleteSongsFromServer(serverid);
-            object request = null;
-            using (Stream fr = Read(ref request))
-            {
-                db.MergeInternetXml(serverid, fr);
-            }
-            CloseRead(request);
-        }
-        public override void UploadChanges(AbstractSongDatabase db, int serverid)
-        {
-            InetSongDb xmldb = new InetSongDb();
-            object req1 = null;
-            using (Stream fr = Read(ref req1)) xmldb.ReadXml(fr);
-            CloseRead(req1);
-
-            db.UpdateInternetXml(serverid, xmldb);
-
-            object req2 = null;
-            using (Stream fw = Write(ref req2))
-            {
-                xmldb.WriteXml(fw);
-            }
-            CloseWrite(req2);
-        }
-        public override void UploadWhole(AbstractSongDatabase db, int serverid)
-        {
-            object request = null;
-            using (Stream fw = Write(ref request))
-            {
-                db.CreateInternetXml(serverid, fw);
-            }
-            CloseWrite(request);
-        }
-
-    }
-
-    public class FtpSongServer : ReadWriteServer
-    {
-        FtpAccess m_access;
-
-        public FtpSongServer(string url, string config)
-            : base("ftp", url, config)
-        {
-            m_access = FtpAccess.Load(config);
-        }
+        [DisplayName("Hostitel")]
+        [Description("Jméno FTP serveru, napø. ftp.webzdarma.cz")]
+        public string Host { get { return m_access.Host; } set { m_access.Host = value; } }
+        [DisplayName("Jméno uživatele")]
+        public string Login { get { return m_access.Login; } set { m_access.Login = value; } }
+        [DisplayName("Heslo")]
+        public string Password { get { return m_access.Password; } set { m_access.Password = value; } }
+        [DisplayName("Cesta k souboru")]
+        [Description("Cesta k souboru na FTP serveru, vìtšinou staèí uvést jméno souboru, napø. muj_zpevnik.xml")]
+        public string Path { get { return m_access.Path; } set { m_access.Path = value; } }
 
         protected override Stream Read(ref object request)
         {
@@ -345,99 +281,14 @@ namespace zp8
         {
             return m_access.UploadFile();
         }
-
-    }
-
-    public class FtpSongServerFactory : PropertyPageBase, ISongServerFactory
-    {
-        FtpAccess m_access = new FtpAccess();
-
-        [DisplayName("Hostitel")]
-        [Description("Jméno FTP serveru, napø. ftp.webzdarma.cz")]
-        public string Host { get { return m_access.Host; } set { m_access.Host = value; } }
-        [DisplayName("Jméno uživatele")]
-        public string Login { get { return m_access.Login; } set { m_access.Login = value; } }
-        [DisplayName("Heslo")]
-        public string Password { get { return m_access.Password; } set { m_access.Password = value; } }
-        [DisplayName("Cesta k souboru")]
-        [Description("Cesta k souboru na FTP serveru, vìtšinou staèí uvést jméno souboru, napø. muj_zpevnik.xml")]
-        public string Path { get { return m_access.Path; } set { m_access.Path = value; } }
-
-        #region ISongServerFactory Members
-
-        public bool Work(List<ISongServer> servers, out string message)
+        public override string ToString()
         {
-            servers.Add(new FtpSongServer(m_access.MakeUrl(), m_access.ToString()));
-            message = "Pøidáno:" + m_access.ToString();
-            return true;
-        }
-
-        #endregion
-    }
-
-    public class FtpSongServerFactoryType : ISongServerFactoryType
-    {
-        #region ISongServerFactoryType Members
-
-        public ISongServerFactory CreateFactory()
-        {
-            return new FtpSongServerFactory();
-        }
-
-        public string Name
-        {
-            get { return "ftp"; }
-        }
-
-        public string Description
-        {
-            get { return "XML soubor uložený na FTP (ètení/zápis)"; }
-        }
-
-        #endregion
-    }
-
-    public class FileSongServerType : ISongServerType
-    {
-        #region ISongServerType Members
-
-        public ISongServer Load(string url, string config)
-        {
-            return new FileSongServer(url);
-        }
-
-        public string Name
-        {
-            get { return "file"; }
-        }
-
-        public bool Readonly
-        {
-            get { return false; }
-        }
-
-        #endregion
-    }
-
-    public class FileSongServer : ReadWriteServer
-    {
-        public FileSongServer(string fileName)
-            : base("file", fileName, null)
-        {
-        }
-
-        protected override Stream Read(ref object request)
-        {
-            return new FileStream(URL, FileMode.Open);
-        }
-
-        protected override Stream Write(ref object request)
-        {
-            return new FileStream(URL, FileMode.Create);
+            return m_access.MakeUrl();
         }
     }
 
-    public class FileSongServerFactory : PropertyPageBase, ISongServerFactory
+    [SongServer(Name = "file", Title = "Soubor na disku", Description = "Soubor uložený na disku, hlavnì pro testovací úèely, chová se stejnì jako FTP song-server", ReadOnly=false)]
+    public class FileSongServer : ReadWriteSongServer
     {
         string m_fileName;
 
@@ -445,38 +296,18 @@ namespace zp8
         [Description("Úplná cesta k souboru na disku, napø. c:/dokumenty/muj_zpevnik.xml")]
         public string FileName { get { return m_fileName; } set { m_fileName = value; } }
 
-        #region ISongServerFactory Members
-
-        public bool Work(List<ISongServer> servers, out string message)
+        protected override Stream Read(ref object request)
         {
-            servers.Add(new FileSongServer(m_fileName));
-            message = "Server pøidán";
-            return true;
+            return new FileStream(FileName, FileMode.Open);
         }
 
-        #endregion
-    }
-
-    public class FileSongServerFactoryType : ISongServerFactoryType
-    {
-        #region ISongServerFactoryType Members
-
-        public ISongServerFactory CreateFactory()
+        protected override Stream Write(ref object request)
         {
-            return new FileSongServerFactory();
+            return new FileStream(FileName, FileMode.Create);
         }
-
-        public string Name
+        public override string ToString()
         {
-            get { return "file"; }
+            return m_fileName;
         }
-
-        public string Description
-        {
-            get { return "Soubor uložený na disku, hlavnì pro testovací úèely, chová se stejnì jako FTP song-server"; }
-        }
-
-        #endregion
     }
 }
-#endif
