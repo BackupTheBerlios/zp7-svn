166aebd6ac770a92ffe11a88f554ae75366d3eba
jena <jena@b4e1c19d-8504-0410-bfc1-910685b3975c>
2007-07-30 16:01:20 +0000

    hlavni menu-napoveda

--- zp8/doc/build.bat
+++ zp8/doc/build.bat
@@ -4,6 +4,9 @@ copy img\*.png %outputdir%\img
 
 xsltproc xml2html.xslt start.xml>%outputdir%\start.html
 xsltproc xml2html.xslt inetdb.xml>%outputdir%\inetdb.html
+xsltproc xml2html.xslt mainmenu.xml>%outputdir%\mainmenu.html
+xsltproc xml2html.xslt bookstyles.xml>%outputdir%\bookstyles.html
+xsltproc xml2html.xslt filters.xml>%outputdir%\filters.html
 
 xsltproc xml2hhc.xslt zp8-hhc.xml>%outputdir%\zp8.hhc
 copy zp8.hhp %outputdir%\zp8.hhp
